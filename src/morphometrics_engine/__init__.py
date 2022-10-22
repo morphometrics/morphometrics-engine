90bfd2d4f383f7bacb80cbe2a821f0b44c970a85
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-10-22 17:06:34 +0200

    initial migration

--- src/morphometrics_engine/__init__.py
+++ src/morphometrics_engine/__init__.py
@@ -1,10 +1,47 @@
+from typing import Callable, List, Optional
+
+from toolz import curry
+
+_measurements = dict()
+
+
 try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
-from ._widget import ExampleQWidget, example_magic_widget
 
-__all__ = (
-    "ExampleQWidget",
-    "example_magic_widget",
-)
+
+@curry
+def register_measurement(
+    func: Callable,
+    name: Optional[str] = None,
+    uses_intensity_image: bool = True,
+) -> Callable:
+    _measurements[name] = {
+        "type": "single",
+        "callable": func,
+        "choices": None,
+        "intensity_image": uses_intensity_image,
+    }
+    return func
+
+
+@curry
+def register_measurement_set(
+    func: Callable,
+    choices: List[str],
+    name: Optional[str] = None,
+    uses_intensity_image: bool = True,
+) -> Callable:
+    _measurements[name] = {
+        "type": "set",
+        "callable": func,
+        "choices": choices,
+        "intensity_image": uses_intensity_image,
+    }
+    return func
+
+
+def available_measurements() -> List[str]:
+    """Get the names of all available measurements."""
+    return [k for k in _measurements]
