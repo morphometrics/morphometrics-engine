54af2b3c8f9ad33a7e526ddb9852928704b5dc5a
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-10-23 10:38:37 +0200

    Initial migration (#1)
    * initial migration
    
    * fix imports
    
    * add example
    
    * add register single test
    
    * add measurement set test
    
    * add types tests
    
    * fix registration tests
    
    * add widget tests

--- src/morphometrics_engine/_tests/test_widget.py
+++ src/morphometrics_engine/_tests/test_widget.py
@@ -1,36 +1,95 @@
 import numpy as np
+import pandas as pd
+from skimage.measure import regionprops_table
 
-from morphometrics_engine import ExampleQWidget, example_magic_widget
+from morphometrics_engine import register_measurement, register_measurement_set
+from morphometrics_engine._widget import QtMeasurementWidget
+from morphometrics_engine.types import (
+    IntensityImage,
+    LabelImage,
+    LabelMeasurementTable,
+)
 
 
-# make_napari_viewer is a pytest fixture that returns a napari viewer object
-# capsys is a pytest fixture that captures stdout and stderr output streams
-def test_example_q_widget(make_napari_viewer, capsys):
+def test_qt_measurement_widget(make_napari_viewer):
+    @register_measurement(name="measure_area", uses_intensity_image=False)
+    def measure_area(label_image: LabelImage) -> LabelMeasurementTable:
+        region_props = regionprops_table(
+            label_image, properties=("label", "area")
+        )
+
+        return pd.DataFrame(region_props).set_index("label")
+
+    @register_measurement_set(
+        name="region_props",
+        uses_intensity_image=True,
+        choices=["intensity", "centroid"],
+    )
+    def measure_region_props(
+        label_image: LabelImage,
+        intensity_image: IntensityImage,
+        intensity: bool = True,
+        centroid: bool = True,
+    ) -> LabelMeasurementTable:
+        base_measurement = regionprops_table(
+            label_image, properties=("label",)
+        )
+        base_table = pd.DataFrame(base_measurement).set_index("label")
+
+        if intensity is True:
+            area_measurements = regionprops_table(
+                label_image,
+                intensity_image=intensity_image,
+                properties=("label", "intensity_mean"),
+            )
+            area_table = pd.DataFrame(area_measurements).set_index("label")
+            base_table = pd.concat([base_table, area_table], axis=1)
+
+        if centroid is True:
+            centroid_measurements = regionprops_table(
+                label_image, properties=("label", "centroid")
+            )
+            centroid_table = pd.DataFrame(centroid_measurements).set_index(
+                "label"
+            )
+            base_table = pd.concat([base_table, centroid_table], axis=1)
+
+        return base_table
+
     # make viewer and add an image layer using our fixture
     viewer = make_napari_viewer()
-    viewer.add_image(np.random.random((100, 100)))
+    image_layer = viewer.add_image(np.random.random((10, 10, 10)))
 
     # create our widget, passing in the viewer
-    my_widget = ExampleQWidget(viewer)
+    measurement_widget = QtMeasurementWidget(viewer)
 
-    # call our widget method
-    my_widget._on_click()
+    label_im = np.zeros((10, 10, 10), dtype=int)
+    label_im[5:10, 5:10, 5:10] = 1
+    label_im[5:10, 0:5, 0:5] = 2
+    label_im[0:5, 0:10, 0:10] = 3
 
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == "napari has 1 layers\n"
+    labels_layer = viewer.add_labels(label_im)
 
+    # set the label image
+    measurement_widget._select_layers(
+        intensity_image=image_layer, label_image=labels_layer
+    )
 
-def test_example_magic_widget(make_napari_viewer, capsys):
-    viewer = make_napari_viewer()
-    layer = viewer.add_image(np.random.random((100, 100)))
+    # select the area measurement
+    measurement_widget.measurement_widgets[0]._check_box.setChecked(True)
 
-    # this time, our widget will be a MagicFactory or FunctionGui instance
-    my_widget = example_magic_widget()
+    # select all of the choices in regionprops
+    regionprops_widget = measurement_widget.measurement_widgets[1]
+    for widget in regionprops_widget._choice_widgets:
+        widget._check_box.setChecked(True)
 
-    # if we "call" this object, it'll execute our function
-    my_widget(viewer.layers[0])
+    # simulate running the measurement
+    measurement_widget._run()
 
-    # read captured output and check that it's as we expected
-    captured = capsys.readouterr()
-    assert captured.out == f"you have selected {layer}\n"
+    # check that the measurements were added
+    feature_table = labels_layer.features
+    assert "area" in feature_table.columns
+    assert "intensity_mean" in feature_table.columns
+    assert "centroid-0" in feature_table.columns
+    assert "centroid-1" in feature_table.columns
+    assert "centroid-2" in feature_table.columns
--- src/morphometrics_engine/_widget.py
+++ src/morphometrics_engine/_widget.py
@@ -1,46 +1,210 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
+import warnings
+from typing import Any, Dict, List, Union
 
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
+import napari
+import numpy as np
+from magicgui import magicgui
+from napari_skimage_regionprops._table import add_table
+from qtpy.QtWidgets import (
+    QCheckBox,
+    QHBoxLayout,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+from superqt.collapsible import QCollapsible
 
-Replace code below according to your needs.
-"""
-from typing import TYPE_CHECKING
-
-from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
-
-if TYPE_CHECKING:
-    import napari
+from morphometrics_engine import _measurements, measure_selected
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
+class QtSingleMeasurement(QWidget):
+    def __init__(self, name: str):
         super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
+        self._measurement_name = name
+        self._label_widget = QLabel(name)
+        self._check_box = QCheckBox()
 
         self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
+        self.layout().addWidget(self._check_box)
+        self.layout().addWidget(self._label_widget)
 
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
+        self.layout().setSpacing(0)
+        self.layout().setContentsMargins(0, 0, 0, 0)
+
+    @property
+    def measurement_name(self) -> str:
+        return self._measurement_name
+
+    @property
+    def include_measurement(self) -> bool:
+        return self._check_box.isChecked()
 
 
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+class QtMeasurementSet(QWidget):
+    def __init__(self, name: str, choices: List[str]):
+        super().__init__()
+        self._measurement_name = name
+
+        self._measurement_section = QCollapsible(
+            title=self._measurement_name, parent=self
+        )
+        self._choice_widgets = [
+            QtSingleMeasurement(name=choice) for choice in choices
+        ]
+        for choice_widget in self._choice_widgets:
+            self._measurement_section.addWidget(choice_widget)
+
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self._measurement_section)
+
+        self.layout().setSpacing(0)
+        self.layout().setContentsMargins(0, 0, 0, 0)
+
+        self._measurement_section.layout().layout().setSpacing(0)
+        self._measurement_section.layout().setContentsMargins(0, 0, 0, 0)
+
+    @property
+    def measurement_name(self) -> str:
+        return self._measurement_name
+
+    @property
+    def include_measurement(self) -> bool:
+        return np.any(
+            [widget.include_measurement for widget in self._choice_widgets]
+        )
+
+    @property
+    def measurement_choices(self) -> Dict[str, bool]:
+        return {
+            widget.measurement_name: widget.include_measurement
+            for widget in self._choice_widgets
+        }
 
 
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def create_measurement_widgets(
+    measurements: List[Dict[str, Any]]
+) -> List[Union[QtSingleMeasurement, QtMeasurementSet]]:
+    widgets = []
+    for measurement_name, measurement_config in measurements.items():
+        if measurement_config["type"] == "single":
+            widgets.append(QtSingleMeasurement(name=measurement_name))
+        elif measurement_config["type"] == "set":
+            widgets.append(
+                QtMeasurementSet(
+                    name=measurement_name,
+                    choices=measurement_config["choices"],
+                )
+            )
+    return widgets
+
+
+class QtMeasurementWidget(QWidget):
+    def __init__(self, viewer: napari.Viewer):
+        super().__init__()
+        self._viewer = viewer
+
+        # create the layer selection widgets
+        self._layer_selection_widget = magicgui(
+            self._select_layers,
+            intensity_image={"choices": self._get_image_layers},
+            label_image={"choices": self._get_labels_layers},
+            auto_call=True,
+            call_button=False,
+        )
+        self._layer_selection_widget()
+
+        # create the measurement widgets
+        self.measurement_widgets = create_measurement_widgets(_measurements)
+
+        self._run_button = QPushButton("Run", self)
+        self._run_button.clicked.connect(self._run)
+
+        # add widgets to the layout
+        self.setLayout(QVBoxLayout())
+        self.layout().addWidget(self._layer_selection_widget.native)
+        for widget in self.measurement_widgets:
+            self.layout().addWidget(widget)
+        self.layout().addWidget(self._run_button)
+
+    @property
+    def measurement_selection(self) -> List[Union[str, Dict[str, Any]]]:
+        measurement_selection = []
+        for widget in self.measurement_widgets:
+            if not widget.include_measurement:
+                continue
+            if isinstance(widget, QtSingleMeasurement):
+                measurement_selection.append(widget.measurement_name)
+            elif isinstance(widget, QtMeasurementSet):
+                measurement_selection.append(
+                    {
+                        "name": widget.measurement_name,
+                        "choices": widget.measurement_choices,
+                    }
+                )
+            else:
+                raise TypeError(
+                    "Widgets should be QtSingleMeasurement or QtMeasurementSet"
+                )
+
+        return measurement_selection
+
+    def _select_layers(
+        self,
+        intensity_image: napari.layers.Image,
+        label_image: napari.layers.Labels,
+    ):
+        self._intensity_image_layer = intensity_image
+        self._label_image_layer = label_image
+
+    def _get_image_layers(self, combo_widget):
+        return [
+            layer
+            for layer in self._viewer.layers
+            if isinstance(layer, napari.layers.Image)
+        ]
+
+    def _get_labels_layers(self, combo_widget):
+        return [
+            layer
+            for layer in self._viewer.layers
+            if isinstance(layer, napari.layers.Labels)
+        ]
+
+    def _run(self):
+        for widget in self.measurement_widgets:
+            print(f"{widget.measurement_name}: {widget.include_measurement}")
+
+            widget_is_set = isinstance(widget, QtMeasurementSet)
+            print(f"{widget.measurement_name} is set: {widget_is_set}")
+
+        labels = self._label_image_layer.data
+
+        if self._intensity_image_layer is not None:
+            image = self._intensity_image_layer.data
+
+            # deal with dimensionality of data
+            if len(image.shape) > len(labels.shape):
+                dim = 0
+                subset = ""
+                while len(image.shape) > len(labels.shape):
+                    current_dim_value = self._viewer.dims.current_step[dim]
+                    dim = dim + 1
+                    image = image[current_dim_value]
+                    subset = subset + ", " + str(current_dim_value)
+                warnings.warn(
+                    "Not the full image was analysed, just the subset ["
+                    + subset[2:]
+                    + "] according to selected timepoint / slice."
+                )
+
+        else:
+            image = None
+        measurement_table = measure_selected(
+            label_image=labels,
+            intensity_image=image,
+            measurement_selection=self.measurement_selection,
+        )
+
+        self._label_image_layer.properties = measurement_table.reset_index()
+        add_table(self._label_image_layer, self._viewer)
